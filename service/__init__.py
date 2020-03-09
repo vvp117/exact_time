2a845201ff02eb96e23e8a504d2ac233d2b2aaec
vvp117 <ponomarev117@gmail.com>
2020-03-09 18:59:45 +0700

    Обновление config из переменных среды

--- service/__init__.py
+++ service/__init__.py
@@ -1,8 +1,8 @@
-from os import environ, path
+from os import path
 
 from quart_openapi import Pint
 
-import service.config as default_config
+from service import config
 
 
 app = Pint(__name__, title='Exact Time Service',
@@ -10,12 +10,7 @@ app = Pint(__name__, title='Exact Time Service',
            base_model_schema=path.join(path.dirname(__file__),
                                        'base_model_schema.json')
            )
-app.config.from_object(default_config)
-
-
-config_file = environ.get('QUART_CONFIG')
-if config_file:
-    app.config.from_pyfile(config_file)
+app.config.from_object(config)
 
 
 import service.view
--- service/config.py
+++ service/config.py
@@ -2,7 +2,7 @@ DEBUG = False
 TESTING = False
 
 # Генерация: import os; print(os.urandom(16))
-SECRET_KEY = 'dev-secret-key'
+SECRET_KEY = ''
 
 # Эта настройка требуется, чтобы получить
 # тексты ошибок от сервера кириллицей без проблем.
@@ -10,6 +10,39 @@ SECRET_KEY = 'dev-secret-key'
 # при отображении
 JSON_AS_ASCII = False
 
-NTP_SERVER = '<ntp-server>'  # for example: 'ntp1.stratum1.ru'
+NTP_SERVER = ''  # for example: 'ntp1.stratum1.ru'
 NTP_PORT = 123
 NTP_VERSION = 4  # 3 or 4, current - 4
+
+
+# Функция для переопределения значений этого
+# файла конфигурации из переданных переменных среды
+def override_from_env_vars():
+    from os import environ
+    from sys import modules
+
+    #  Overriding configuration file values
+    def booler(var):
+        return True if var.lower() == 'true' else False
+
+    converters = {
+        'DEBUG': booler,
+        'TESTING': booler,
+        'NTP_PORT': int,
+        'NTP_VERSION': int,
+    }
+
+    thismodule = modules[__name__]
+
+    for var_name in dir(thismodule):
+        if not var_name.isupper():
+            continue
+
+        env_var = environ.get(var_name)
+
+        if env_var:
+            converter = converters.get(var_name, str)
+            setattr(thismodule, var_name, converter(env_var))
+
+
+override_from_env_vars()
